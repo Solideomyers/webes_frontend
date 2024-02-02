1b21c4eb26da1437389e32815047c4170dd60424
Solideomyers <franciscomyers@gmail.com>
2024-02-02 19:43:47 +0100

    wip: add central pages

---
+++ src/pages/index.ts
@@ -0,0 +1,5 @@
+export { Home } from './home/Home.tsx';
+export { CategoriaView } from './categoriapage/Categoria.tsx';
+export { ProductoView } from './productopage/Producto.tsx';
+export { NotfoundView } from './notfoundpage/Notfound.tsx';
+export { ContactView } from './contact/Contact.tsx';
