5ab6577e35fa7468d35a1a05da407f2e19570d8b
Solideomyers <franciscomyers@gmail.com>
2024-02-02 18:48:58 +0100

    feat: add footerhead component

---
+++ src/components/footer/FooterHead.tsx
@@ -0,0 +1,12 @@
+export const FooterHead = () => {
+  return (
+    <div className='mb-6 md:mb-0'>
+      <a href='/' className='flex items-center'>
+        <img src='/src/assets/logo_gris.png' className='mr-3 h-8' alt='Logo' />
+        {/* <span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>
+                La Web del Colchón
+              </span> */}
+      </a>
+    </div>
+  );
+};
