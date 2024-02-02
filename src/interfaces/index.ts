e83243babea12765b4bca095249238466d46e224
Solideomyers <franciscomyers@gmail.com>
2024-02-02 19:37:18 +0100

    feat: add central interfaces

---
+++ src/interfaces/index.ts
@@ -0,0 +1,9 @@
+export { type Categorias, type Datum } from './categorias/categorias';
+export { type Product, type ProductsByCat } from './productos/products';
+export {
+  type ProductID,
+  type Img,
+  type Related,
+  type ProductDetail,
+  type Atributo,
+} from './productos/productid';
