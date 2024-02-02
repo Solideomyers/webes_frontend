315d5d6d844d6fd23d2c05d11f00230a5f252f94
Solideomyers <franciscomyers@gmail.com>
2024-02-02 19:13:01 +0100

    wip: add central components

---
+++ src/components/index.ts
@@ -0,0 +1,52 @@
+// header
+export { NavBar } from './navbar/NavBar';
+export { Banner } from './banner/Banner';
+export { Hero } from './hero/Hero';
+export { CardHero } from './hero/CardHero';
+
+// utils
+export { Breadcrumbs } from './breadcrumbs/Breadcrumbs';
+export { Notfound } from './notfound/Notfound';
+export { Loading } from './loading/Loading';
+export { LoadingIcon } from './loading/LoadingIcon';
+export { ProductoSk } from './skeleton/ProductoSk';
+export { CategoriaSk } from './skeleton/CategoriaSk';
+
+// productos
+export { Productos } from './producto/Productos';
+export { Producto } from './producto/Producto';
+export { ProductoDetail } from './producto/ProductoDetail';
+export { SeccionDetailR } from './producto/SeccionDetailR';
+export { ProductosSlide } from './producto/ProductosSlide';
+export { ProductosRelated } from './producto/ProductosRelated';
+export { Accordeon } from './accordeon/Accordeon';
+
+// categorias
+export { Categoria } from './categoria/Categoria';
+export { Categorias } from './categoria/Categorias';
+
+// footer
+export { Footer } from './footer/Footer';
+export { FooterPie } from './footer/FooterPie';
+export { Enlaces } from './footer/Enlaces';
+export { FooterHead } from './footer/FooterHead';
+
+export { Testimonials } from './testimonials/Testimonials';
+export { Testimonial } from './testimonials/Testimonial';
+export { Tabinfo } from './tabinfo/Tabinfo';
+
+export { Cart } from './cart/Cart';
+export { Form } from './contact/Form';
+export { Filter } from './filter/Filter';
+
+// UI
+export { Input } from './ui/Input';
+export { Button } from './ui/Button';
+export { ButtonSecondary } from './ui/ButtonSecondary';
+export { Label } from './ui/Label';
+export { ComboBoxUi } from './ui/ComboBoxUi';
+export { DropdownUi } from './ui/DropdownUi';
+export { FavoriteButton } from './ui/FavoriteButton';
+export { SocialIcons } from './ui/SocialIcons';
+export { Reviews } from './ui/Reviews';
+export { TooltipUi } from './ui/TooltipUi';
