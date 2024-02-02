1dc8e66fca7dc023cf883abd3f92378d7efd0210
Solideomyers <franciscomyers@gmail.com>
2024-02-02 19:10:46 +0100

    wip: add reviews component

---
+++ src/components/ui/Reviews.tsx
@@ -0,0 +1,51 @@
+import { Tooltip } from "keep-react";
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const Reviews: React.FC = () => {
+  return (
+    <>
+      <Link to={"#"}>
+        <Tooltip content={"Te invitamos a leer nuestras reseñas"}>
+          <span className="flex items-center">
+            <svg
+              fill="currentColor"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="w-4 h-4 text-primary"
+              viewBox="0 0 24 24"
+            >
+              <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+            </svg>
+            <svg
+              fill="currentColor"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="w-4 h-4 text-primary"
+              viewBox="0 0 24 24"
+            >
+              <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+            </svg>
+
+            <svg
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="w-4 h-4 text-primary"
+              viewBox="0 0 24 24"
+            >
+              <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+            </svg>
+            <span className="text-gray-600 ml-3">4 Reviews</span>
+          </span>
+        </Tooltip>
+      </Link>
+    </>
+  );
+};
