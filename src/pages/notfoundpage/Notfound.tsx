cc6e51db9297897a4621f346b430495fe62718c9
Solideomyers <franciscomyers@gmail.com>
2024-02-02 19:40:07 +0100

    wip: add notfound view

---
+++ src/pages/notfoundpage/Notfound.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Notfound } from '../../components';
+export const NotfoundView: React.FC = () => {
+  return (
+    <div>
+      <Notfound />
+    </div>
+  );
+};
