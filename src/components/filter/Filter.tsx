5338c3344033a509804269860d42fb947577a8dc
Solideomyers <franciscomyers@gmail.com>
2024-02-02 18:59:01 +0100

    wip: add filter component

---
+++ src/components/filter/Filter.tsx
@@ -0,0 +1,33 @@
+export const Filter = () => {
+  return (
+    <>
+      <h3 className='mb-4 font-semibold text-text-color'>Filter</h3>
+      <ul className='grid grid-cols-4 place-content-between border border-[#ccc]'>
+        <li>
+          <div>
+            <input />
+            <label></label>
+          </div>
+        </li>
+        <li>
+          <div>
+            <input />
+            <label></label>
+          </div>
+        </li>
+        <li>
+          <div>
+            <input />
+            <label></label>
+          </div>
+        </li>
+        <li>
+          <div>
+            <input />
+            <label></label>
+          </div>
+        </li>
+      </ul>
+    </>
+  );
+};
