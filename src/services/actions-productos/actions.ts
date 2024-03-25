4e44a394d7296b698a5858d48e52a754f709da45
Solideomyers <franciscomyers@gmail.com>
2024-03-25 12:26:33 +0100

    fix: fixed url

--- src/services/actions-productos/actions.ts
+++ src/services/actions-productos/actions.ts
@@ -5,6 +5,8 @@ export const getProducto = async (
   idp: string,
   ido: string
 ): Promise<ProductID> => {
-  const { data } = await categoriasApi.get(`/productos/${idp}?ida=${ido}`);
+  const { data } = await categoriasApi.get(
+    `/productos/${idp ? idp : 554}?ida=${ido ? ido : 97005}`
+  );
   return data;
 };
