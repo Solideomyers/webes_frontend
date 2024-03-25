49ebf4253ddf7e30f9bb09cca93d0f31643e76de
Solideomyers <franciscomyers@gmail.com>
2024-03-25 13:51:16 +0100

    fix: refactor

--- src/components/categoria/Categorias.tsx
+++ src/components/categoria/Categorias.tsx
@@ -1,41 +1,33 @@
 'use client';
-import React, { memo, useEffect } from 'react';
-import { useInView } from 'react-intersection-observer';
-import { Categoria, CategoriaSk, LoadingIcon } from '..';
-import {
-  useCategorias,
-  useCustomNavigate,
-  usePrefetchCatByPro,
-} from '../../hooks';
+import React, { Fragment, memo, useEffect, useMemo } from 'react';
+import { useCustomNavigate, usePrefetchCatByPro } from '../../hooks';
 import { type Datum } from '../../interfaces';
-
+import { Categoria } from './Categoria';
+import { CategoriaSk } from '../skeleton/CategoriaSk';
+import { Button } from '@nextui-org/react';
+import { useProductContext } from '../../context/ProductContext';
+import { GridBoxOne } from '../dashboard/GridBoxOne';
+import { v4 as uuidv4 } from 'uuid';
 const CategoriaSkMemo = memo(CategoriaSk);
+const CategoriaMemo = memo(Categoria);
 
 export const Categorias: React.FC = () => {
-  const { ref, inView } = useInView({ threshold: 0, delay: 1000 });
-  const { categoriasInfiniteQuery } = useCategorias();
+  const { categoriasInfiniteQuery, setCatId } = useProductContext();
   const { prefetchProductsByCat } = usePrefetchCatByPro();
   const { handleRoute } = useCustomNavigate();
-  const categoriasData = categoriasInfiniteQuery?.data;
+  const categoriasData = useMemo(
+    () => categoriasInfiniteQuery?.data,
+    [categoriasInfiniteQuery.data]
+  );
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
-  useEffect(() => {
-    categoriasInfiniteQuery.fetchNextPage();
-  }, []);
-
-  useEffect(() => {
-    if (inView) {
-      categoriasInfiniteQuery.fetchNextPage();
-    }
-  }, [inView, categoriasInfiniteQuery.hasNextPage]);
-
-  if (categoriasInfiniteQuery.isLoading || categoriasInfiniteQuery.isPending) {
+  if (categoriasInfiniteQuery.isLoading) {
     return (
       <div className='grid grid-cols-1 md:grid-cols-4 gap-4 mt-4'>
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((_, i: number) => (
+        {[1, 2, 3, 4, 5, 6].map((_, i: number) => (
           <div key={i} className='w-full'>
             <CategoriaSkMemo key={i} />
           </div>
@@ -48,31 +40,60 @@ export const Categorias: React.FC = () => {
     return <div>Error al carga categorías</div>;
   }
 
+  const handleSelectionChange = (key: React.Key) => {
+    setCatId(String(key));
+  };
+  // const esPar = (num: number): boolean => num % 2 === 0;
   return (
-    <section className='bg-white dark:bg-gray-900 dark:text-gray-100 mx-10'>
-      <h1 className='font-bold my-4 text-2xl text-neutral-900'>Categorías</h1>
-      <div className='grid grid-cols-1 md:grid-cols-4 gap-4'>
-        {categoriasData?.pages.map((page, i: number) =>
-          page?.data?.data.map((cat: Datum, j: number) => (
-            <div key={`${i}-${j}`} className='grid gap-4'>
-              <Categoria
-                prefetchProductsByCat={prefetchProductsByCat}
-                handleRoute={() => handleRoute('/categoria/', cat.id)}
-                id={cat.id}
-                name={cat.name}
-              />
+    <section className='col-span-2 bg-white dark:bg-gray-900 dark:text-gray-100 mx-10'>
+      <h1 className='font-bold mt-2 mb-1 text-2xl text-neutral-900'>
+        Categorías
+      </h1>
+      <hr className='mb-3 border border-gray-300 ' />
+      <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+        {categoriasData?.pages.map((page) => (
+          <Fragment key={uuidv4()}>
+            {page?.data?.data.map((cat: Datum, i) => (
+              <div key={i} className={`gap-2`}>
+                <CategoriaMemo
+                  idimg={uuidv4()}
+                  prefetchProductsByCat={prefetchProductsByCat}
+                  handleRoute={() => handleRoute('/categoria/', cat.id)}
+                  id={cat.id}
+                  setCatId={handleSelectionChange}
+                  name={cat.name}
+                />
+              </div>
+            ))}
+            <div className='grid grid-cols-subgrid col-span-1 sm:col-span-3 lg:col-span-2 lg:grid-flow-col-dense'>
+              <div className='row-span-2 col-span-1 lg:row-start-1 py-0 sm:col-span-3 lg:col-span-2 grid grid-cols-2 gap-2 rounded-xl w-full '>
+                <GridBoxOne
+                  title='50% OFF'
+                  name1='Cabecero'
+                  name2='Tapizado Julie'
+                  price={300.0}
+                  titleButton='Comprar'
+                  isolated={true}
+                />
+              </div>
             </div>
-          ))
-        )}
+          </Fragment>
+        ))}
       </div>
-      <button
-        ref={ref}
+      <Button
+        variant='solid'
+        color='primary'
+        radius='lg'
+        role='button'
+        onClick={() => categoriasInfiniteQuery.fetchNextPage()}
         className={`${
-          categoriasInfiniteQuery.data?.pages.length === 0 ? 'hidden' : ''
-        }`}
+          categoriasInfiniteQuery.data?.pages.length === 4 ? 'hidden' : ''
+        } my-2`}
       >
-        {categoriasInfiniteQuery.isFetchingNextPage ? <LoadingIcon /> : ''}
-      </button>
+        Cargar mas
+      </Button>
+
+      {/* <Galeries /> */}
     </section>
   );
 };
