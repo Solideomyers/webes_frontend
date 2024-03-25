f8ed81707825c8f4736a8ad2477bd9b2f01487ab
Solideomyers <franciscomyers@gmail.com>
2024-03-25 13:58:03 +0100

    fix: refactor query infinite

--- src/components/producto/Productos.tsx
+++ src/components/producto/Productos.tsx
@@ -1,49 +1,44 @@
 import { useCallback, useEffect, useState } from 'react';
-import { useInView } from 'react-intersection-observer';
+import { formatDescription, usePrefetchProduct } from '../../hooks';
+import { type Datum, type ProductsByCat } from '../../interfaces';
+import { TabsCat } from '../categoria/TabsCat';
+import { useProductContext } from '../../context/ProductContext';
 import { useParams } from 'react-router-dom';
-import {
-  formatDescription,
-  useCategoriaId,
-  usePrefetchProduct,
-} from '../../hooks';
-import { Filter, LoadingIcon, Producto, ProductoSk } from '..';
-import { ProductsByCat } from '../../interfaces';
+import { ProductoSk } from '../skeleton/ProductoSk';
+import { Button } from '@nextui-org/react';
+import { useFetchNextPage } from '../../hooks/categorias/useFetchNextPage';
+import { useLoadAllCategories } from '../../hooks/categorias/useLoadAllCategories';
+import { Producto } from './Producto';
 
 export const Productos: React.FC = () => {
-  const { id } = useParams<{ id: string }>();
-  const { categoriaIdQuery } = useCategoriaId(Number(id) || 1);
+  const { categoriaIdQuery, categoriasInfiniteQuery } = useProductContext();
+  const { id } = useParams();
   const initialData = 6;
   const [perView, setPerView] = useState(initialData);
-  const { ref, inView } = useInView({ threshold: 0, delay: 2000 });
   const { prefetchProduct } = usePrefetchProduct();
+
+  const categorias = categoriasInfiniteQuery.data;
+
+  const fetchNextPage = useFetchNextPage({
+    categorias,
+    categoriasInfiniteQuery,
+  });
+
+  const categoriasAllData = useLoadAllCategories({ fetchNextPage, categorias });
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
-  useEffect(() => {
-    if (inView && perView < (categoriaIdQuery.data?.totalProducts || 0)) {
-      setPerView(perView + initialData);
-    }
-  }, [inView]);
-
-  if (!categoriaIdQuery.data) {
-    return (
-      <section className='flex flex-col items-center mx-4 bg-background-color-component/5'>
-        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
-          {[...Array(initialData)].map((_, i) => (
-            <div key={i} className='w-full'>
-              <ProductoSk />
-            </div>
-          ))}
-        </div>
-      </section>
-    );
-  }
-
   // handlemordata
   const handleMoreLoad = useCallback(() => {
     setPerView(perView + initialData);
+  }, [perView]);
+
+  const handleRemove = useCallback(() => {
+    if (perView > initialData) {
+      setPerView(perView - initialData);
+    }
   }, [perView]);
 
   const products = categoriaIdQuery?.data?.data?.products_by_cat
@@ -54,34 +49,66 @@ export const Productos: React.FC = () => {
 
   return (
     <section className='flex flex-col items-center mx-4 bg-background-color-component/5'>
-      <Filter />
-      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
-        {products?.map((cat: ProductsByCat, i: number) => (
-          <div key={i} className='grid gap-4'>
-            <Producto
-              id_category={id || ''}
-              idp={cat.idp}
-              id_o={cat.id_o}
-              cat_name={cat.cat_name}
-              attribute_price={cat.attribute_price}
-              proname={cat.proname}
-              offer={false}
-              description={formatDescription(cat.description)}
-              prefetchProduct={prefetchProduct}
-            />
-          </div>
-        ))}
+      <TabsCat
+        defaultKey={id || ''}
+        data={(categoriasAllData as Datum[]) || []}
+      />
+
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 place-items-center py-2'>
+        {categoriaIdQuery.isLoading ||
+        categoriaIdQuery.data?.data.products_by_cat.length == 0 ? (
+          <>
+            {[...Array(initialData)].map((_, i) => (
+              <div key={i} className='w-full'>
+                <ProductoSk />
+              </div>
+            ))}
+          </>
+        ) : (
+          products?.map((cat: ProductsByCat, i: number) => (
+            <div key={i} className=''>
+              <Producto
+                id_category={id || ''}
+                idp={cat.idp}
+                id_o={cat.id_o}
+                cat_name={cat.cat_name}
+                attribute_price={cat.attribute_price}
+                proname={cat.proname}
+                offer={false}
+                description={formatDescription(cat.description)}
+                prefetchProduct={prefetchProduct}
+              />
+            </div>
+          ))
+        )}
       </div>
-      <div
-        className={`w-full mt-4 ${
-          categoriaIdQuery.data?.totalProducts === perView ? 'hidden' : ''
-        }`}
-      >
-        <button ref={ref} onClick={handleMoreLoad}>
-          {(categoriaIdQuery?.data?.totalProducts || 0) > perView && (
-            <LoadingIcon />
-          )}
-        </button>
+      <div className='flex gap-4'>
+        <Button
+          // ref={ref}
+          variant='solid'
+          color='primary'
+          radius='lg'
+          role='button'
+          onClick={handleMoreLoad}
+          className={`${
+            (categoriaIdQuery.data?.totalProducts || 0) <= perView
+              ? 'hidden'
+              : ''
+          } my-2`}
+        >
+          Cargar mas
+        </Button>
+        <Button
+          // ref={ref}
+          variant='bordered'
+          color='primary'
+          radius='lg'
+          role='button'
+          onClick={handleRemove}
+          className={`${perView <= initialData ? 'hidden' : ''} my-2`}
+        >
+          Cargar menos
+        </Button>
       </div>
     </section>
   );
