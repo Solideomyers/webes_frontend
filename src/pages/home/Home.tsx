e136c8ea163261b0217db07e9ae83cfec8aa8cc3
Solideomyers <franciscomyers@gmail.com>
2024-03-25 12:55:29 +0100

    fix: refactor styles

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,17 +4,22 @@ import {
   ProductosSlide,
   Testimonials,
 } from '../../components';
+import { GalleryHome } from '../../components/galleryhome/GalleryHome';
+import SingleMap from '../../components/maps/MapsWeb';
 
 export const Home: React.FC = () => {
   return (
-    <section className='mx-auto py-2 grid grid-cols-3 gap-y-2 '>
+    <section className='mx-auto py-2 grid grid-cols-3 gap-y-2 overflow-hidden'>
       <header className='col-span-3'>
         <Hero />
       </header>
-      <main className='grid col-span-4 grid-flow-rows-dense place-items-center overflow-hidden mt-2'>
+      <main className='col-span-3 grid grid-rows-1 grid-cols-2 place-items-center'>
         <ProductosSlide />
         <Categorias />
+
+        <GalleryHome />
         <Testimonials />
+        {/* <SingleMap /> */}
       </main>
     </section>
   );
