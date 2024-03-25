201960e7174e8e14fc1a742deb7fabde1766ee6d
Solideomyers <franciscomyers@gmail.com>
2024-03-25 13:40:38 +0100

    fix: refactor breadcrumbs and styles

--- src/App.tsx
+++ src/App.tsx
@@ -17,12 +17,12 @@ function App() {
   return (
     <ProductProvider>
       <section>
-        <nav className='z-50 sticky top-0 flex flex-col gap-0 drop-shadow-[0_20px_35px_rgba(0,0,0,0.25)]'>
+        <nav className='z-50 sticky top-0 flex flex-col gap-0 drop-shadow-[0_20px_35px_rgba(0,0,0,0.25)] w-full'>
           <Banner />
           <NavBar />
         </nav>
         <main className='grid grid-cols-1 mx-auto px-1'>
-          {location.pathname !== '/' ? <Breadcrumbs /> : null}
+          {location.pathname.includes('/categoria') ? <Breadcrumbs /> : ''}
           <Outlet />
         </main>
         <footer>
