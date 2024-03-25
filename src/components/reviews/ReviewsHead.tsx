061823bd4f0295b1edd21e5ad2b21bc0dab948f7
Solideomyers <franciscomyers@gmail.com>
2024-03-25 15:55:34 +0100

    feat: create component

---
+++ src/components/reviews/ReviewsHead.tsx
@@ -0,0 +1,10 @@
+export const ReviewHead = () => {
+  return (
+    <div className='flex flex-col w-full'>
+      <h1 className='font-bold text-gray-800 '>Reseñas</h1>
+      <span>
+        La excelencia de nuestros productos contada por nuestros clientes
+      </span>
+    </div>
+  );
+};
