aa1dfbb0414867bb4722cf0ab48ea3cf90838a96
Solideomyers <franciscomyers@gmail.com>
2024-03-25 19:17:22 +0100

    wip: refactor

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,22 +1,45 @@
 'use client';
 import { useCallback, useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import { useInView } from 'react-intersection-observer';
-import { Button, Navbar } from 'keep-react';
-import logo from '../../assets/logo_gris.png';
-import { Heart, ShoppingCart, User } from 'phosphor-react';
-import { MenuMobile } from './MenuMobile';
 import { useProductContext } from '../../context/ProductContext';
-import { useCategorias } from '../../hooks';
 import { type Categorias, type Datum } from '../../interfaces';
-import { ComboBoxUi } from '..';
+import {
+  Badge,
+  Button,
+  Link,
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle,
+  Tooltip,
+} from '@nextui-org/react';
+import { ComboBoxUi } from '../ui/ComboBoxUi';
+import { Logo } from './Logo';
+import { RiLoginCircleFill } from 'react-icons/ri';
+import { SlBasketLoaded } from 'react-icons/sl';
+import { SlBasket } from 'react-icons/sl';
 
 export const NavBar: React.FC = () => {
-  const { products, setProducts } = useProductContext();
-  const [isOpen, setIsOpen] = useState(false);
-  const { categoriasInfiniteQuery } = useCategorias();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { products, categoriasInfiniteQuery, setCatId } = useProductContext();
   const [isCat, setIsCat] = useState<Datum[]>([]);
   const { ref, inView } = useInView({ threshold: 0 });
+
+  const menuItems = [
+    'Profile',
+    'Dashboard',
+    'Activity',
+    'Analytics',
+    'System',
+    'Deployments',
+    'My Settings',
+    'Team Settings',
+    'Help & Feedback',
+    'Log Out',
+  ];
 
   const dispatch = useCallback(() => {
     if (inView && categoriasInfiniteQuery.hasNextPage) {
@@ -41,71 +64,108 @@ export const NavBar: React.FC = () => {
     updateCategorias();
   }, [updateCategorias]);
 
+  const handleSelectionChange = (key: React.Key) => {
+    setCatId(String(key));
+  };
+
   return (
-    <Navbar fluid={true}>
-      <Navbar.Container className='flex items-center justify-between'>
-        {/* logo */}
-        <Navbar.Brand>
-          <Link to={'/'}>
-            <img src={logo} alt='keep' width='100' height='40' />
-          </Link>
-        </Navbar.Brand>
-        {/* barra de busqueda */}
-        <Navbar.Container
-          tag='ul'
-          className='lg:flex items-center justify-between z-50'
-        >
-          <ComboBoxUi categories={isCat} innerRef={ref} />
-        </Navbar.Container>
-        {/* menu desplegable */}
-        {/* <Navbar.Collapse
-          className={`${
-            isOpen ? 'hidden' : ''
-          } backdrop-blur-xl backdrop-filter bg-white bg-opacity-80 absolute`}
-          collapseType='sidebar'
-        >
-          <Navbar.Container tag='ul' className='flex flex-col gap-5'>
-            <Navbar.Link linkName='Home' />
-            <Navbar.Link linkName='Projects' />
-            <Navbar.Link linkName='Blogs' />
-            <Navbar.Link linkName='News' />
-            <Navbar.Link linkName='Resources' />
-          </Navbar.Container>
-        </Navbar.Collapse> */}
+    <Navbar
+      className='justify-self-end'
+      isBordered
+      maxWidth='full'
+      isBlurred={false}
+      isMenuOpen={isMenuOpen}
+      onMenuOpenChange={setIsMenuOpen}
+    >
+      <NavbarContent className='sm:hidden pr-3' justify='start'>
+        <NavbarBrand>
+          <Logo />
+          {/* <p className='font-bold text-inherit'>ACME</p> */}
+        </NavbarBrand>
+      </NavbarContent>
 
-        {/* iconos */}
-        <Navbar.Container className='flex items-center gap-3'>
-          <Navbar.Container
-            tag='ul'
-            className='lg:flex hidden items-center justify-between gap-5'
-          >
-            <Navbar.Link
-              icon={<User size={20} color='#444' />}
-              iconAnimation={false}
-            />
-            <Navbar.Link
-              icon={<Heart size={20} color='#444' />}
-              iconAnimation={false}
-            />
-          </Navbar.Container>
+      <NavbarContent className='hidden sm:flex gap-4' justify='center'>
+        <NavbarBrand>
+          <Logo />
+        </NavbarBrand>
+        <NavbarContent>
+          <ComboBoxUi
+            categories={isCat}
+            setCatId={handleSelectionChange}
+            innerRef={ref}
+          />
+        </NavbarContent>
+      </NavbarContent>
 
-          <Button
-            href='/cart'
-            className='hidden lg:flex'
-            size='xs'
-            type='outlineGray'
-          >
-            <span>
-              <ShoppingCart size={20} color='#444' />
-            </span>
-            <span className='ml-1 text-primary'>
-              {`${products.length}` || '0'}
-            </span>
-          </Button>
-          <Navbar.Toggle />
-          <MenuMobile />
-        </Navbar.Container>
-      </Navbar.Container>
+      <NavbarContent className='' justify='end'>
+        <NavbarContent justify='end'>
+          {/* <NavbarItem className='hidden border border-red-500 lg:flex'>
+            <Link href='#'>Login</Link>
+          </NavbarItem> */}
+          <NavbarItem className='flex items-center'>
+            <Tooltip content='Accede a tu cuenta' color='primary'>
+              <Button
+                isIconOnly
+                size='sm'
+                radius='md'
+                as={Link}
+                className='bg-primary/95 text-white mr-2'
+                href='/signin'
+                variant='solid'
+              >
+                <RiLoginCircleFill size={20} />
+              </Button>
+            </Tooltip>
+            {/* <Tooltip content='Productos en la cesta' color='primary'> */}
+            <div className='flex justify-center items-center font-bold p-2 border-2 border-primary/90 text-primary hover:border-white hover:text-white hover:bg-[#94c0e5] transition-all ease-in-out delay-75 duration-75 rounded-xl'>
+              <Badge
+                variant='solid'
+                className='border border-[#cccccc] bg-white text-primary'
+                size='sm'
+                content={products.length}
+                color='warning'
+                placement='bottom-right'
+              >
+                {products.length ? (
+                  <div>
+                    <SlBasketLoaded size={18} className='' />
+                  </div>
+                ) : (
+                  <div>
+                    <SlBasket size={18} />
+                  </div>
+                )}
+              </Badge>
+            </div>
+            {/* </Tooltip> */}
+          </NavbarItem>
+        </NavbarContent>
+        <NavbarMenuToggle
+          className='sm:hidden'
+          aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+        />
+      </NavbarContent>
+
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              className='w-full'
+              color={
+                index === 2
+                  ? 'warning'
+                  : index === menuItems.length - 1
+                  ? 'danger'
+                  : 'foreground'
+              }
+              href='#'
+              size='lg'
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
     </Navbar>
   );
 };
