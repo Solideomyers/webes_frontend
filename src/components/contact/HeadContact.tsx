648bd41b9c245a3c9e4492cd32e0866d51c0bec0
Solideomyers <franciscomyers@gmail.com>
2024-03-25 13:45:58 +0100

    feat: added component

---
+++ src/components/contact/HeadContact.tsx
@@ -0,0 +1,9 @@
+export const HeadContact: React.FC = () => {
+  return (
+    <div className='w-full flex justify-center'>
+      <h1 className='font-bold text-heading-4 text-[#2d2a2afb]'>
+        La Web del Colchon
+      </h1>
+    </div>
+  );
+};
