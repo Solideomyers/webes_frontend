77656af20dc2922f03fa723a96f47a6742602188
Solideomyers <franciscomyers@gmail.com>
2024-03-25 19:25:06 +0100

    feat: added nextUi

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import * as React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import { router } from './router';
+import { NextUIProvider } from '@nextui-org/react';
 import { TanStackProvider } from './plugins/TanStackProvider';
 import { RouterProvider } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <TanStackProvider>
-      <RouterProvider router={router} />
+      <NextUIProvider>
+        <RouterProvider router={router} />
+      </NextUIProvider>
     </TanStackProvider>
   </React.StrictMode>
 );
