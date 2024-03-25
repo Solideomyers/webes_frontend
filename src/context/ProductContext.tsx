0456eb41429ddeaaa7e3c5874794812b3ad24b6c
Solideomyers <franciscomyers@gmail.com>
2024-03-25 16:40:53 +0100

    fix: refactor

--- src/context/ProductContext.tsx
+++ src/context/ProductContext.tsx
@@ -1,10 +1,34 @@
-import React, { createContext, useContext, ReactNode } from 'react';
-import { ProductsByCat } from '../interfaces';
-import { useLocalStorage } from '../hooks';
+import React, { createContext, useContext, ReactNode, useState } from 'react';
+import { Categorias, Product, ProductID, ProductsByCat } from '../interfaces';
+import {
+  useCategoriaId,
+  useCategorias,
+  useLocalStorage,
+  useProducto,
+} from '../hooks';
+import {
+  InfiniteData,
+  UseInfiniteQueryResult,
+  UseQueryResult,
+} from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
 
 interface ProductContextProps {
   products: ProductsByCat[];
   setProducts: React.Dispatch<React.SetStateAction<ProductsByCat[]>>;
+  categoriasQuery: UseQueryResult<Categorias, Error>;
+  categoriasInfiniteQuery: UseInfiniteQueryResult<
+    InfiniteData<Categorias, unknown>,
+    Error
+  >;
+  categoriaIdQuery: UseQueryResult<Product, Error>;
+  productoQuery: UseQueryResult<ProductID, Error>;
+  id: string | undefined;
+  idp: string | undefined;
+  ido: string | undefined;
+  setCatId: React.Dispatch<React.SetStateAction<string | undefined>>;
+  catId: string | undefined;
+  handleSelectionChange?: (key: React.Key) => void;
 }
 
 const ProductContext = createContext<ProductContextProps | undefined>(
@@ -14,10 +38,33 @@ const ProductContext = createContext<ProductContextProps | undefined>(
 export const ProductProvider: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
+  const { id, idp, ido } = useParams<{
+    id: string;
+    idp: string;
+    ido: string;
+  }>();
+  const [catId, setCatId] = useState(id);
   const [products, setProducts] = useLocalStorage('product', []);
+  const { categoriasQuery, categoriasInfiniteQuery } = useCategorias();
+  const { categoriaIdQuery } = useCategoriaId(catId || '');
+  const { productoQuery } = useProducto(idp || '', ido || '');
 
   return (
-    <ProductContext.Provider value={{ products, setProducts }}>
+    <ProductContext.Provider
+      value={{
+        products,
+        setProducts,
+        categoriasQuery,
+        categoriasInfiniteQuery,
+        categoriaIdQuery,
+        productoQuery,
+        id,
+        idp,
+        ido,
+        catId,
+        setCatId,
+      }}
+    >
       {children}
     </ProductContext.Provider>
   );
