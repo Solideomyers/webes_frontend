77d164ef6f09191acfa4d96ce8e9b5f0bc6f7923
Solideomyers <franciscomyers@gmail.com>
2024-03-25 13:36:34 +0100

    fix: refactor

--- src/components/ui/DropdownUi.tsx
+++ src/components/ui/DropdownUi.tsx
@@ -1,12 +1,16 @@
-import React, { Fragment } from 'react';
-import { Listbox, Transition } from '@headlessui/react';
-import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid';
-import { Atributo } from '../../interfaces';
+import React from 'react';
+import { type Atributo } from '../../interfaces';
+import {
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+} from '@nextui-org/react';
+import { Selected } from '../../interfaces/productos/productid';
 
-interface Props {
+interface Props extends Selected {
   atributos: Atributo[];
-  selected: Atributo;
-  setSelected: React.Dispatch<React.SetStateAction<Atributo>>;
 }
 
 export const DropdownUi: React.FC<Props> = ({
@@ -14,65 +18,20 @@ export const DropdownUi: React.FC<Props> = ({
   selected,
   setSelected,
 }) => {
-  // console.log(selected);
-
   return (
-    <Listbox value={selected} onChange={setSelected}>
-      <section className='flex justify-start items-center gap-4'>
-        <span className='font-semibold'>Elegir medida: </span>
-        <div className='relative mt-1'>
-          <Listbox.Button className='relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm'>
-            <span className='block truncate'>
-              {selected?.attr_title?.split('x').join('x')}
-            </span>
-            <span className='pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2'>
-              <ChevronUpDownIcon
-                className='h-5 w-5 text-gray-400'
-                aria-hidden='true'
-              />
-            </span>
-          </Listbox.Button>
-          <Transition
-            as={Fragment}
-            leave='transition ease-in duration-100'
-            leaveFrom='opacity-100'
-            leaveTo='opacity-0'
-          >
-            <Listbox.Options className='absolute z-20 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm'>
-              {atributos?.map((person, personIdx) => (
-                <Listbox.Option
-                  key={personIdx}
-                  className={({ active }) =>
-                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                      active ? 'bg-primary text-white' : 'text-gray-900'
-                    }`
-                  }
-                  value={person}
-                >
-                  {({ selected }) => (
-                    <>
-                      <span
-                        className={`block truncate ${
-                          selected ? 'font-medium' : 'font-normal'
-                        }`}
-                      >
-                        {person.attr_title.split('x').join('x')}
-                      </span>
-                      {selected ? (
-                        <span className='absolute inset-y-0 left-0 flex items-center pl-3 text-white'>
-                          <CheckIcon className='h-5 w-5' aria-hidden='true' />
-                        </span>
-                      ) : null}
-                    </>
-                  )}
-                </Listbox.Option>
-              ))}
-            </Listbox.Options>
-          </Transition>
-        </div>
-      </section>
-    </Listbox>
-    // <div className='fixed top-16 w-72'>
-    // </div>
+    <Dropdown>
+      <DropdownTrigger>
+        <Button variant='bordered' color='primary'>
+          Medidas
+        </Button>
+      </DropdownTrigger>
+      <DropdownMenu aria-label='actions menu'>
+        {atributos?.map((item) => (
+          <DropdownItem onClick={() => setSelected(item)} key={item.id_o}>
+            {item.attr_title}
+          </DropdownItem>
+        ))}
+      </DropdownMenu>
+    </Dropdown>
   );
 };
