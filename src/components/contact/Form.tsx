b6644a0e1c1723c037904be1ccd1fd9c10c357ed
Solideomyers <franciscomyers@gmail.com>
2024-03-25 16:50:49 +0100

    wip: refactor

--- src/components/contact/Form.tsx
+++ src/components/contact/Form.tsx
@@ -1,59 +1,89 @@
-import { Button } from '..';
+import React from 'react';
+import { Button, Input, Textarea } from '@nextui-org/react';
+import { FaUserCircle } from 'react-icons/fa';
+import { MdEditSquare } from 'react-icons/md';
 
-export const Form = () => {
+export const Form: React.FC = () => {
   return (
-    <section className=' bg-transparent w-full max-w-xl  dark:bg-gray-900'>
-      <div className='bg-background-color py-8 lg:py-16 border-2 rounded-xl px-4 mx-auto max-w-screen-md'>
-        <form action='#' className='space-y-8'>
-          <div>
-            <label
-              htmlFor='email'
-              className='block mb-2 text-sm font-medium text-text-color dark:text-gray-300'
-            >
-              Email
-            </label>
-            <input
-              type='email'
-              id='email'
-              className='shadow-sm bg-gray-50 border border-background-color-component text-text-color text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
-              placeholder='name@flowbite.com'
-              required
-            />
-          </div>
-          <div>
-            <label
-              htmlFor='subject'
-              className='block mb-2 text-sm font-medium text-text-color dark:text-gray-300'
-            >
-              Asunto
-            </label>
-            <input
-              type='text'
-              id='subject'
-              className='block p-3 w-full text-sm text-text-color bg-gray-50 rounded-lg border border-background-color-component shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
-              placeholder='Ayudanos a saber como podemos ayudarte'
-              required
-            />
-          </div>
-          <div className='sm:col-span-2'>
-            <label
-              htmlFor='message'
-              className='block mb-2 text-sm font-medium text-text-color dark:text-gray-400'
-            >
-              Mensaje
-            </label>
-            <textarea
-              id='message'
-              rows={6}
-              className='block p-2.5 w-full text-sm text-text-color bg-gray-50 rounded-lg shadow-sm border border-background-color-component focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
-              placeholder='Deja tus comentarios...'
-            ></textarea>
-          </div>
-          <Button variant='primary' type='submit'>
-            Enviar
+    <div className='col-span-2 sm:col-span-1 bg-[#ffffff] shadow-md shadow-[#2d2c2c34] rounded-xl p-4 flex'>
+      <form className='flex flex-col gap-2 w-full'>
+        <div className='p-2 text-[#1a1919ee]'>
+          <h1 className='mb-2 font-bold text-lg'>Formulario de Contacto</h1>
+          <p className='font-regular'>
+            No dudes en escribirnos tus consultas o sugerencias y tan pronto nos
+            sea posible nuestro esquipo de atencion al cliente atendera tus
+            requerimientos
+          </p>
+        </div>
+        <div className='flex  flex-col gap-4'>
+          <Input
+            variant='bordered'
+            color='primary'
+            isRequired
+            label='Nombre'
+            placeholder='Escribe tu nombre'
+            labelPlacement='outside'
+          />
+          <Input
+            variant='bordered'
+            color='primary'
+            isRequired
+            label='Email'
+            placeholder='Escribe tu email'
+            labelPlacement='outside'
+          />
+          <Input
+            variant='bordered'
+            color='primary'
+            isRequired
+            label='Email'
+            placeholder='Escribe tu email'
+            labelPlacement='outside'
+          />
+          <Textarea
+            variant='bordered'
+            color='primary'
+            isRequired
+            label='Mensaje'
+            labelPlacement='outside'
+            placeholder='Escribe tu mensaje'
+          />
+        </div>
+        <Button variant='solid' color='primary'>
+          Enviar
+        </Button>
+        <>
+          <p className='font-medium text-sm my-2 bg-background-color-component/40 rounded-lg p-2'>
+            Si aun no has creado tu cuenta puedes hacerlo{' '}
+            <em className='text-contrast-accent-color-dark'>ahora</em>
+          </p>
+          <Button
+            fullWidth
+            size='sm'
+            startContent={<FaUserCircle />}
+            variant='flat'
+            color='primary'
+          >
+            Crear Cuenta
           </Button>
-        </form>
-      </div>
-    </section>
+        </>
+        <>
+          <p className='font-medium text-sm my-2 bg-background-color-component/40 rounded-lg p-2'>
+            Si ya has comprado alguno de nuestros productos dale amor a nuestros
+            productos con una linda resena{' '}
+            <em className='text-contrast-accent-color-dark'>desde aqui</em>
+          </p>
+          <Button
+            fullWidth
+            size='sm'
+            startContent={<MdEditSquare />}
+            variant='bordered'
+            color='primary'
+          >
+            Crear Resena
+          </Button>
+        </>
+      </form>
+    </div>
   );
 };
